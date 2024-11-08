bb8bf4af2eb41545b9ee2ac26d1c2654768f87eb
Sanket-aws-devops <157350422+Sanket-aws-devops@users.noreply.github.com>
2024-11-08 16:18:23 +0530

    Update WebConfig.java

--- src/main/java/com/transfort/management/transport/config/WebConfig.java
+++ src/main/java/com/transfort/management/transport/config/WebConfig.java
@@ -4,17 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @Configuration  // Marks this class as a configuration class
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        // Configuring CORS for all endpoints in the application
-        registry.addMapping("/**")  // Apply CORS to all endpoints
-                .allowedOrigins("*")  // Allowed origins (change as needed)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
-                .allowedHeaders("*")  // Allow any headers
-                .allowCredentials(true);  // Allow credentials (cookies, authorization headers)
+  @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOriginPatterns("*").allowCredentials(true)
+                        .allowedMethods("*").allowedHeaders("*");
+            }
+        };
     }
 }
